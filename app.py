cb841a7e5d614f3c0538475a08657d06c097dece
elison <>
2024-06-09 16:48:44 +0700

    fix chromium error

--- app.py
+++ app.py
@@ -32,10 +32,13 @@ if "article" not in st.session_state:
 # options.add_argument("--headless")  # Chạy dưới dạng headless (không hiển thị trình duyệt)
 
 #New setting
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+@st.cache_resource
+def init_webdriver():
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return driver
 
 
 # -------------------------------- 1.Working with OpenAI and Langchain ------------------------
@@ -84,12 +87,13 @@ def get_article_content(url):
     return article
 
 #Keep text only
-@st.cache_data
+# @st.cache_data
 def get_website_content(url):
+    driver = init_webdriver()
     driver.get(url)
     time.sleep(5)
     html_doc = driver.page_source
-    driver.quit()
+    # driver.quit()
     soup = BeautifulSoup(html_doc, "html.parser")
     return soup.get_text()
 
@@ -160,7 +164,7 @@ def site_extraction_page():
                     st.write(content)
         with content_col2:
             with st.container(border=True):
-                with st.spinner("Đang chức lại thông tin..."):
+                with st.spinner("Đang tổ chức lại thông tin..."):
                     summary_content = extract_chain.invoke({"content":content})
                     st.write(summary_content)
 
